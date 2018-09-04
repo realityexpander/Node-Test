f1ddee77591bb5db171247548810af2c69f53740
Chris Athanas <realityexpander@gmail.com>
2018-09-04 14:43:57 -0500

    change update functionality
    Change from GET to POST

--- nodetest.js
+++ nodetest.js
@@ -3,7 +3,7 @@ var dt = require('./DateTime');
 var url = require('url');
 var fs = require('fs');
 var http = require('http');
-var qs = require('querystring');
+var {parse} = require('querystring');
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -24,51 +24,57 @@ con.connect(function(err) {
       return;
 
     var request = url.parse(req.url, true).query;
-    var requestTable = request.table;
     var requestPath = url.parse(req.url, true).pathname;
 
-    // Default pathname
+    // Route: Default path
     if (requestPath === '/') {
-      requestTable = 'employee';
+      request.table = 'employee';
       request.column = 'emp_id';
       request.item = '%';
-      viewDatabase(requestPath, requestTable, request.column, request.item);
+      viewDatabase(requestPath, request); //.table, request.column, request.item);
     }
 
-    // Edit pathname
+    // Route: Edit an Element
     if (requestPath === "/edit") {
-      requestTable        = request.table;
-      var requestIDColumn = request.IDColumn;
-      var requestIDItem   = request.IDItem;
-      var editColumn      = request.editColumn;
-      var newData         = request.newData;
-      var requestColumn   = request.previousColumn; // from previous search
-      var requestItem     = request.previousItem; // from previous search
-      var query = 'UPDATE ' + requestTable 
-                + ' SET ' + editColumn 
-                + ' = "' + newData + '"' 
-                + ' WHERE ' + requestIDColumn 
-                + ' = "' + requestIDItem + '"';
-      con.query(query, function(err, result) {
-        console.log(result);
-        if (err) {
-          res.write(err);
-          res.end();
-          return;
-        }
+      if (req.method === 'POST') {
+        collectRequestData(req, request => {
+          console.log(request);
+          //res.write(`Parsed data belonging to ${JSON.stringify(request)}`);
 
-        viewDatabase(requestPath, requestTable, requestColumn, requestItem);
-      });
+          var query = 'UPDATE ' + request.table 
+                    + ' SET '   + request.editColumn 
+                    + ' = "'    + request.newData + '"' 
+                    + ' WHERE ' + request.IDColumn 
+                    + ' = "'    + request.IDItem + '"';
+          con.query(query, function(err, result) {
+            console.log(result);
+            if (err) {
+              res.write(err);
+              res.end();
+              return;
+            } else {
+              viewDatabase(requestPath, request); //.table, request.previousColumn, request.previousItem);
+            }
+          });
+        });
+      } else { // show default data, if user attempts a refresh on Edit
+        request.table = 'employee';
+        request.column = 'emp_id';
+        request.item = '%';
+        viewDatabase(requestPath, request); //.table, request.column, request.item);
+      }
+
     }
 
+    // Route: View table
     // http://localhost:8075/view?database=employee&emp_id=10_
     if (requestPath === '/view') {
-      viewDatabase(requestPath, requestTable, request.column, request.item);
+      viewDatabase(requestPath, request); //.table, request.column, request.item);
     }
 
-    function viewDatabase(requestPath, requestTable, requestColumn, requestItem) {
+    function viewDatabase(requestPath, request) { //request.table, requestColumn, requestItem) {
       //var query = 'SELECT employee.first_name, employee.last_name FROM employee WHERE employee.emp_id IN (SELECT works_with.emp_id FROM works_with WHERE works_with.total_sales > 30000)'
-      var query = 'SELECT * FROM ' + requestTable + ' WHERE ' + requestColumn + ' LIKE ?';
+      var query = 'SELECT * FROM ' + request.table + ' WHERE ' + request.column + ' LIKE ?';
 
       // Set the Headers and Head tags for css
       res.writeHead(200, {
@@ -78,9 +84,10 @@ con.connect(function(err) {
       res.write(data);
 
       con.query('SHOW TABLES', function(err, result) {
+        // Get the tables for this database
         // Heading
         res.write(`<h1>${requestPath}:${JSON.stringify(request)}</h1>`);
-        res.write('<h3>' + query + '[' + requestItem + ']</h3>');
+        res.write(`<h3>${query}[${request.item}]</h3>`);
 
         // Show available tables
         var availableTables = '';
@@ -91,13 +98,13 @@ con.connect(function(err) {
 
         // Search box
         res.write('<form action="/view" method="GET" target="_self">');
-        res.write('<b>' + availableTables + '</b><br>');
+        res.write(`<b>${availableTables}</b><br>`);
         res.write('Search Table: ');
-        res.write(`<input type="text" name="table"  value="${requestTable}"     required></input><br>`);
+        res.write(`<input type="text" name="table"  value="${request.table}"  required></input><br>`);
         res.write('Search column: ');
-        res.write(`<input type="text" name="column" value="${requestColumn}" required></input><br>`);
+        res.write(`<input type="text" name="column" value="${request.column}" required></input><br>`);
         res.write('Search criteria: ');
-        res.write(`<input type="text" name="item"   value="${requestItem}"   required></input><br>`);
+        res.write(`<input type="text" name="item"   value="${request.item}"   required></input><br>`);
         res.write('<input type="submit" value="View"><br>');
         res.write('</form>');
         res.write('</td>');
@@ -110,8 +117,8 @@ con.connect(function(err) {
         res.write('<tbody>');
         res.write('<tr>');
 
-        // Get all the tables in the database
-        con.query('DESCRIBE ' + requestTable, [requestItem], function(err, result) {
+        // Get all the columns in the database
+        con.query('DESCRIBE ' + request.table, [request.item], function(err, result) {
           for (var i in result) {
             res.write('<th>');
             res.write(result[i].Field);
@@ -119,16 +126,16 @@ con.connect(function(err) {
           }
           res.write('</tr>');
 
-          // Show the records
+          // Show the rows
           // http://localhost:8075/view?database=employee&emp_id=10_
-          con.query(query, [requestItem], function(err, result) {
+          con.query(query, [request.item], function(err, result) {
             if (err) {
               res.write('</tbody>');
               res.write('</table>');
               res.write('<br>' + err + '<br>');
             } else {
 
-              // Show edit table
+              // Show data and edit table UI
               for (var i in result) {
                 // res.write("<p>Record: "+ i + "=" + JSON.stringify(result[i]) +"</p><br>");
                 res.write('<tr>');
@@ -143,14 +150,14 @@ con.connect(function(err) {
                   res.write('<td>');
                   res.write('' + data);
                   //http://localhost:8075/edit?newData=David&table=employee&IDColumn=emp_id&IDItem=100&editColumn=first_name&previousColumn=emp_id&previousItem=%25
-                  res.write('<form action="/edit" method="GET" target="_self">');
-                  res.write(`<input type="text" name="newData" value="${data}" required</input>`);
-                  res.write(`<input type="hidden" id="table"          name="table"          value="${requestTable}">`);
+                  res.write('<form action="/edit" method="POST" target="_self">');
+                  res.write(`<input type="text"                       name="newData"        value="${data}" required</input>`);
+                  res.write(`<input type="hidden" id="table"          name="table"          value="${request.table}">`);
                   res.write(`<input type="hidden" id="IDColumn"       name="IDColumn"       value="${fields[0]}">`);
                   res.write(`<input type="hidden" id="IDItem"         name="IDItem"         value="${result[i][fields[0]]}">`);
                   res.write(`<input type="hidden" id="editColumn"     name="editColumn"     value="${fields[j]}">`);
-                  res.write(`<input type="hidden" id="previousColumn" name="previousColumn" value="${requestColumn}">`);
-                  res.write(`<input type="hidden" id="previousItem"   name="previousItem"   value="${requestItem}">`);
+                  res.write(`<input type="hidden" id="previousColumn" name="previousColumn" value="${request.column}">`);
+                  res.write(`<input type="hidden" id="previousItem"   name="previousItem"   value="${request.item}">`);
                   res.write('<input type="submit" value="Update">');
                   res.write('</form>');
                   res.write('</td>');
@@ -172,6 +179,23 @@ con.connect(function(err) {
   }).listen(8075);
 
 });
+
+function collectRequestData(request, callback) {
+  const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+  if (request.headers['content-type'] === FORM_URLENCODED) {
+    let body = '';
+    request.on('data', chunk=>{
+      body += chunk.toString();
+    }
+    );
+    request.on('end', ()=>{
+      callback(parse(body));
+    }
+    );
+  } else {
+    callback(null);
+  }
+}
 
 // con.connect(function(err) {
 //   if (err) throw err;
